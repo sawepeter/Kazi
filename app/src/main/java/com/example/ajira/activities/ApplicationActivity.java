797e7bae93b87f0b6f8f924eb6f7b7fb1beebcb0
Peter Sawe <peter.sawe@riverbank.co.ke>
2020-09-10 02:30:24 +0300

    added new classes

---
+++ app/src/main/java/com/example/ajira/activities/ApplicationActivity.java
@@ -0,0 +1,16 @@
+package com.example.ajira.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ajira.R;
+
+public class ApplicationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_application);
+    }
+}
--- app/src/main/java/com/example/ajira/activities/DashBoardActivity.java
+++ app/src/main/java/com/example/ajira/activities/DashBoardActivity.java
@@ -2,15 +2,28 @@ package com.example.ajira.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.ajira.R;
 
 public class DashBoardActivity extends AppCompatActivity {
 
+    LinearLayout l1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        l1 = findViewById(R.id.l1);
+        l1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DashBoardActivity.this, JobDetailsActivity.class));
+            }
+        });
     }
 }

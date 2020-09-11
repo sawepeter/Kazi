98d72067b08daa3cdf8f9f112820efa0314c8a76
Peter Sawe <peter.sawe@riverbank.co.ke>
2020-09-11 04:21:30 +0300

    added new classes

--- app/src/main/java/com/example/ajira/activities/DashBoardActivity.java
+++ app/src/main/java/com/example/ajira/activities/DashBoardActivity.java
@@ -8,10 +8,12 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import com.example.ajira.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DashBoardActivity extends AppCompatActivity {
 
     LinearLayout l1;
+    BottomNavigationView bottom_navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,8 @@ public class DashBoardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dash_board);
 
         l1 = findViewById(R.id.l1);
+        bottom_navigation = findViewById(R.id.bottom_navigation);
+        bottom_navigation.setSelectedItemId(R.id.Home);
         l1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
